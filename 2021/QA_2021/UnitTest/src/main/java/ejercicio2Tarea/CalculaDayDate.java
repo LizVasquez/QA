215b195294c76018bcf48e1a8ca08ca363a21356
LizVasquez <lizvasmen@gmail.com>
2021-02-19 07:20:19 -0400

    tarea

--- 2021/QA_2021/UnitTest/src/main/java/ejercicio2Tarea/CalculaDayDate.java
+++ 2021/QA_2021/UnitTest/src/main/java/ejercicio2Tarea/CalculaDayDate.java
@@ -1,8 +1,19 @@
 package ejercicio2Tarea;
 
 public class CalculaDayDate {
-    //public String resultado(int day, String month, int year, int hour, int minute, int second) {
-      //  String message;
+    public void resultado(int day, String month, int year, int hour, int minute, int second) {
+       String message;
+       String[] meses = {"enero","febrero","marzo","abril","mayo","junio", "julio",
+       "agosto","septiembre","octubre","noviembre","diciembre"};
 
-    //}
+       if(day <1 || day >31){
+           message = "Dia invalido";
+       }else {
+
+
+       }
+
+
+    }
+
 }
--- 2021/QA_2021/UnitTest/src/test/java/ejercicio2DateDay/CalculaDayDateTest.java
+++ 2021/QA_2021/UnitTest/src/test/java/ejercicio2DateDay/CalculaDayDateTest.java
@@ -33,19 +33,25 @@ public class CalculaDayDateTest {
     public static Iterable<Object[]> getData(){
         List<Object[]> objects = new ArrayList<>();
 
-        objects.add(new Object[]{15,"febrero",2000,13,00,00,"15 febrero 2000 13:00:01"});
+        objects.add(new Object[]{15,"abril",2000,13,00,00,"16 abril 2000 13:00:01"});
         objects.add(new Object[]{31,"diciembre",2020,23,59,59,"1 enero 2020 00:00:00"});
         objects.add(new Object[]{28,"febrero",2021,23,59,59,"1 marzo 2021 00:00:00"});
+        objects.add(new Object[]{28,"febrero",2020,23,59,59,"29 febrero 2020 00:00:00"});
+        //----
         objects.add(new Object[]{32,"noviembre",2018,00,00,00,"Dia invalido"});
         objects.add(new Object[]{-5,"mayo",2000,18,00,00,"Dia invalido"});
-        objects.add(new Object[]{30,"octubre",2001,27,30,00,"Hora  invalida"});
-        objects.add(new Object[]{24,"junio",1999,15,30,65,"Segundo invalido"});
+        //-------
         objects.add(new Object[]{2,"jajaja",1600,23,10,00,"Mes invalido"});
-        objects.add(new Object[]{28,"julio",-1,18,45,32,"Año invalido"});
-        objects.add(new Object[]{18,"marzo",2010,19,75,00,"Minuto invalido"});
         objects.add(new Object[]{2,"marte",1500,23,15,00,"Mes invalido"});
-
-
+        //----------
+        objects.add(new Object[]{28,"julio",015,18,45,32,"Año invalido"});
+        //---------
+        objects.add(new Object[]{30,"octubre",2001,27,30,00,"Hora  invalida"});
+        objects.add(new Object[]{8,"enero",2020,35,30,00,"Hora  invalida"});
+        //----------MIN
+        objects.add(new Object[]{18,"marzo",2010,19,75,00,"Minuto invalido"});
+        //--------SEC
+        objects.add(new Object[]{24,"junio",1999,15,30,65,"Segundo invalido"});
 
         return objects;
 
